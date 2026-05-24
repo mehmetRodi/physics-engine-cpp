c9355374624d6c2e3114919e1c62363a0175910a
mehmetRodi <rodiaydogdu22@gmail.com>
2026-05-24 22:01:40 +0300

    rigid body unit tests

--- tests/physics/rigid_body_tests.cpp
+++ tests/physics/rigid_body_tests.cpp
@@ -1,1 +1,38 @@
+#include "physics/RigidBody.hpp"
 #include <gtest/gtest.h>
+
+TEST(RigidBodyTests, PositiveMassStoresInverseMass) {
+  const RigidBody body(2.0f, 20.0f);
+  EXPECT_FLOAT_EQ(body.invMass, 0.5f);
+}
+
+TEST(RigidBodyTests, ZeroMassBodyHasZeroInverseMass) {
+  const RigidBody body(0.0f, 20.0f);
+  EXPECT_FLOAT_EQ(body.invMass, 0.0f);
+}
+
+TEST(RigidBodyTests, ApplyForceChangesAccelerationByForceTimesInvMass) {
+  RigidBody body(2.0f, 20.0f);
+  body.applyForce(Vec3(4.0f, 0.f, 0.f));
+  EXPECT_FLOAT_EQ(body.acceleration.x, 2.0f);
+  EXPECT_FLOAT_EQ(body.acceleration.y, 0.0f);
+  EXPECT_FLOAT_EQ(body.acceleration.z, 0.0f);
+}
+
+TEST(RigidBodyTests, UpdateUsesSemiImplicitEuler) {
+  RigidBody body(2.0f, 20.0f);
+  body.acceleration = Vec3(4.0f, 0.f, 0.f);
+  body.velocity = Vec3(1.0f, 0.f, 0.f);
+  body.update(1.0f);
+  EXPECT_FLOAT_EQ(body.velocity.x, 5.0f);
+  EXPECT_FLOAT_EQ(body.position.x, 5.0f);
+}
+
+TEST(RigidBodyTests, UpdateClearsAccelerationAfterStep) {
+  RigidBody body(2.0f, 20.0f);
+  body.acceleration = Vec3(4.0f, 0.f, 0.f);
+  body.update(1.0f);
+  EXPECT_FLOAT_EQ(body.acceleration.x, 0.0f);
+  EXPECT_FLOAT_EQ(body.acceleration.y, 0.0f);
+  EXPECT_FLOAT_EQ(body.acceleration.z, 0.0f);
+}
