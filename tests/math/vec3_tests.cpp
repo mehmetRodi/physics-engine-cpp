7633b9489342f413f8b4dd77280d7d2d765f5d4a
mehmetRodi <rodiaydogdu22@gmail.com>
2026-05-24 20:04:24 +0300

    first test working confirming setup correctness

--- tests/math/vec3_tests.cpp
+++ tests/math/vec3_tests.cpp
@@ -1,2 +1,10 @@
 #include "math/Vec3.hpp"
 #include <gtest/gtest.h>
+
+TEST(Vec3Tests, DefaultConstructorStartsAtOrigin) {
+  const Vec3 value;
+
+  EXPECT_FLOAT_EQ(value.x, 0.0f);
+  EXPECT_FLOAT_EQ(value.y, 0.0f);
+  EXPECT_FLOAT_EQ(value.z, 0.0f);
+}
