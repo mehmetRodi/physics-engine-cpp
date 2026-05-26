076f33ed3a100da966bf3c05414ab2f05abb6bcf
mehmetRodi <rodiaydogdu22@gmail.com>
2026-05-26 17:53:19 +0300

    added the damping test

--- tests/physics/rigid_body_tests.cpp
+++ tests/physics/rigid_body_tests.cpp
@@ -36,3 +36,14 @@ TEST(RigidBodyTests, UpdateClearsAccelerationAfterStep) {
   EXPECT_FLOAT_EQ(body.acceleration.y, 0.0f);
   EXPECT_FLOAT_EQ(body.acceleration.z, 0.0f);
 }
+
+TEST(RigidBodyTests, LinearDampingReducesVelocityDuringUpdate) {
+  RigidBody body(1.0f, 20.0f);
+  body.velocity = Vec3(10.0f, 0.0f, 0.0f);
+  body.material.linearDamping = 0.25f;
+
+  body.update(1.0f);
+
+  EXPECT_FLOAT_EQ(body.velocity.x, 7.5f);
+  EXPECT_FLOAT_EQ(body.position.x, 7.5f);
+}
