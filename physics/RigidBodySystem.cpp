bda3547fbcffe8f271fc066a78f3b7739cc1d7dc
mehmetRodi <rodiaydogdu22@gmail.com>
2026-05-27 18:08:26 +0300

    quick performance improvement

--- physics/RigidBodySystem.cpp
+++ physics/RigidBodySystem.cpp
@@ -22,6 +22,7 @@ const RigidBody& RigidBodySystem::rigidBody(RigidBodyId id) const {
 void RigidBodySystem::reserveRigidBodies(std::size_t capacity) {
   m_bodies.reserve(capacity);
   m_sphereProxies.reserve(capacity);
+  m_collisionPairs.reserve(capacity * (capacity - 1) / 2);
 }
 
 void RigidBodySystem::step(float dt, const Vec3& gravity) {
